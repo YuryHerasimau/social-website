72128615abaa673bb07dd41c409bdb38e2a04385
yl.herasimau <yl.herasimau@gmail.com>
2022-10-05 19:02:32 +0300

    Added user registration

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
+from .forms import LoginForm, UserRegistrationForm
 
 
 def user_login(request):
@@ -24,7 +25,9 @@ def user_login(request):
                 return HttpResponse('Invalid login')
     else:
         form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
+    return render(request,
+                'account/login.html',
+                {'form': form})
 
 
 @login_required
@@ -32,3 +35,25 @@ def dashboard(request):
     return render(request,
                 'account/dashboard.html',
                 {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+        # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            # Save the User object
+            new_user.save()
+            return render(request,
+                        'account/register_done.html',
+                        {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                'account/register.html',
+                {'user_form': user_form})
